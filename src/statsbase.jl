62af130ffa5c0ae78b504a7d5268b89f265294c2
José Bayoán Santiago Calderón <naoyabpr@gmail.com>
2019-09-11 22:39:08 -0400

    Using @autodocs

--- src/statsbase.jl
+++ src/statsbase.jl
@@ -177,10 +177,10 @@ function predict(obj::EconometricModel{<:OrdinalResponse})
 end
 """
 	coeftable(obj::EconometricModel;
-			  level::Real = 0.95)
+		  level::Real = 0.95)
 	coeftable(obj::EconometricModel{<:LinearModelEstimators};
-			  level::Real = 0.95,
-			  vce::VCE = obj.vce)
+		  level::Real = 0.95,
+		  vce::VCE = obj.vce)
 
 Return a table of class `CoefTable` with coefficients and related statistics.
 `level` determines the level for confidence intervals (by default, 95%).
