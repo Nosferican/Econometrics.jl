fc70c7cfa2bcb08f9f355b92375e937a2944faee
José Bayoán Santiago Calderón <naoyabpr@gmail.com>
2024-11-21 09:53:27 -0500

    Some updating and quick fix docs (#88)
    * Update docs compat

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,6 +5,14 @@ using MLJBase: MLJBase
 using MLJModelInterface: MLJModelInterface
 using WooldridgeDatasets: wooldridge
 
-for file in ["exceptions", "linear_models", "mlogit", "ologit", "formula_display", "mlj", "docs"]
+for file in [
+    "exceptions",
+    "linear_models",
+    "mlogit",
+    "ologit",
+    "formula_display",
+    "mlj",
+    "docs"
+    ]
     include("$file.jl")
 end
