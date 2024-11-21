e778450e0d81e9efc7baf6ec22e8dafdc5480df6
José Bayoán Santiago Calderón <naoyabpr@gmail.com>
2024-11-21 09:16:29 -0500

    Some updating and quick fix docs

--- test/docs.jl
+++ test/docs.jl
@@ -10,27 +10,31 @@
             file,
             out_path = joinpath(prefix, "docs", "src"),
             doctype = "github"
-            )
+        )
     end
 
-    DocMeta.setdocmeta!(Econometrics,
-                       :DocTestSetup,
-                       :(using Econometrics, Documenter, CSV, RDatasets, StatsBase;
-                         ENV["COLUMNS"] = 120;
-                         ENV["LINES"] = 30;),
-                       recursive = true)
+    DocMeta.setdocmeta!(
+        Econometrics,
+        :DocTestSetup,
+        :(using Econometrics, Documenter, CSV, RDatasets, StatsBase;
+        ENV["COLUMNS"] = 120;
+        ENV["LINES"] = 30;),
+        recursive = true
+    )
     # doctest(Econometrics, fix = true)
-    makedocs(sitename = "Econometrics",
-             format = Documenter.HTML(assets = [joinpath("assets", "custom.css")]),
-             modules = [Econometrics, StatsAPI],
-             pages = [
-                 "Introduction" => "index.md",
-                 "Getting Started" => "getting_started.md",
-                 "Estimators" => "estimators.md",
-                 "API" => "api.md",
-                 ],
-             source = joinpath(prefix, "docs", "src"),
-             build = joinpath(prefix, "docs", "build"),
-             )
+    makedocs(
+        sitename = "Econometrics",
+        format = Documenter.HTML(assets = [joinpath("assets", "custom.css")]),
+        modules = [Econometrics, StatsAPI],
+        pages = [
+            "Introduction" => "index.md",
+            "Getting Started" => "getting_started.md",
+            "Estimators" => "estimators.md",
+            "API" => "api.md",
+        ],
+        source = joinpath(prefix, "docs", "src"),
+        build = joinpath(prefix, "docs", "build"),
+        checkdocs = :none,
+    )
     @test true
 end
